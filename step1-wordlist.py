60af4344e0a50377fe9c9bd4e53119494c8bd6b4
alireza-hariri <alireza.hariri@ut.ac.ir>
2023-07-13 11:27:58 +0330

    write json

--- step1-wordlist.py
+++ step1-wordlist.py
@@ -4,10 +4,14 @@ import json
 with open('data/words.json') as f:
     words = json.load(f)
 
-print(words)
-print(len(words))
+# print(words)
+# print(len(words))
+
+
+for word in words:
+    print(word)
 
 
 
-
-
+with open( 'data/5_char_words.json' , 'w' , encoding='utf8') as f:
+    json.dump(words[:10],f,ensure_ascii=False,indent=4)
