036ff43873b3eefac6fd49e5e8987d09ece1fd92
nima12345nijm <abouee.nima123@gmail.com>
2023-07-16 18:45:20 +0330

    joda kardan kalamat 5 harfi

--- step1-wordlist.py
+++ step1-wordlist.py
@@ -1,13 +1,22 @@
 
 import json
 
-with open('data/words.json') as f:
+kalame = (input("kalame ra hads bezanid"))
+
+with open('data/words.json',encoding="utf8") as f:
     words = json.load(f)
 
-print(words)
-print(len(words))
+L5Words = []
+L4Words = []
 
+for w in words:
+    if len (w) == 4 :
+        L4Words.append( w )
+    if len (w) == 5 :
+        L5Words.append( w )
 
+#print (L5Words)
 
-
+if kalame in words :
+    print ("dorst")
 
